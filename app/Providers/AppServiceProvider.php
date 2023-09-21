75e82ab71780a88337a8094ed260a23f10e400e2
kamal <kamal.zyel@gmail.com>
2023-09-21 16:43:58 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,30 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    config(['app.locale' => 'id']);
+    Carbon::setLocale('id');
+    date_default_timezone_set('Asia/Jakarta');
+    if (config('app.env') === 'production') {
+      \URL::forceScheme('https');
+      request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
     }
+  }
 }
