d2c03165894d3e1d9ea7f370366c011ef234f131
kamal <kamal.zyel@gmail.com>
2024-07-24 14:42:26 +0700

    fix name

--- resources/views/admin/dokumen/index.blade.php
+++ resources/views/admin/dokumen/index.blade.php
@@ -139,7 +139,7 @@
       </div>
       <div class="space-y-1">
         <label for="" class="text-sm font-medium">Tanggal</label>
-        <input name="description" type="date"
+        <input name="created_at" type="date"
           class="w-full px-2 py-2 rounded ring-gray-300 focus:ring-2 focus:ring-blue-400 outline-none ring-1">
       </div>
       <div class="space-y-1">
